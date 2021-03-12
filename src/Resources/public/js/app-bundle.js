a2fa8de0086ce103508c4c47b41621cac19e8efb
German Lescano <galescano@santafe.gov.ar>
2021-03-12 10:42:32 -0300

    - Se agrega autocompletado en los campos del tipo Select - Se agrega descripción del archivo seleccionado a los campos para subir archivos

--- src/Resources/public/js/app-bundle.js
+++ src/Resources/public/js/app-bundle.js
@@ -2,13 +2,23 @@ import $ from 'jquery';
 import 'popper.js';
 import 'bootstrap';
 
+import 'selectize';
+import bsCustomFileInput from 'bs-custom-file-input';
+
 import '../css/app-bundle.scss';
 
 import { toggleAll, bulkSubmitBtnManage } from './petkopara-crud-generator-webpack';
 
-console.log('Hola desde GALesMakerBundle');
+// console.log('Hola desde GALesMakerBundle');
 
 $( () => {
     $('#check-all-thead').on('click', ($e) => toggleAll($e.target) );
     $('.check-all-row').on('click', () => bulkSubmitBtnManage() );
+
+    bsCustomFileInput.init();
+
+    $("select").selectize({
+        create: true,
+        sortField: 'text'
+    });
 })
