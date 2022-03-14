fe557370b8adfbb8527e186f05011b20620f3a72
German Lescano <galescano@santafe.gov.ar>
2022-03-14 11:48:32 -0300

    - Ajustes para crear codigo sin depender de los templates de Twig

--- src/Resources/skeleton/crud/templates/others/flash_messages.tpl.php
+++
@@ -1,9 +0,0 @@
-    <div id="alert-container" class="col-md-12">
-        {% for type, flashMessages in app.session.flashbag.all() %}
-        {% for flashMessage in flashMessages %}
-        <div class="alert alert-{{ type }}">
-            {{ flashMessage|raw }}
-        </div>
-        {% endfor %}
-        {% endfor %}
-    </div>
