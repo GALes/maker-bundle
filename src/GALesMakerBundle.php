fc258cc23fc82f19c066d871ab77493b5e6f4f81
German Lescano <galescano@santafe.gov.ar>
2023-08-30 13:08:10 -0300

    - Se corrige error en archivo de entrada del bundle

--- src/GALesMakerBundle.php
+++ src/GALesMakerBundle.php
@@ -3,11 +3,12 @@
 namespace GALes\MakerBundle;
 
 use GALes\MakerBundle\DependencyInjection\GALesMakerExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class GALesMakerBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if ($this->extension === null) {
             $this->extension = new GALesMakerExtension();
